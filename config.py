2bcf339de5fa3638bf3881fa50908ca0af58b144
I4j4j <157837377+I4j4j@users.noreply.github.com>
2024-01-28 16:22:39 +0200

    Add files via upload

---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+
+
+class Config(object):
+    TG_BOT_TOKEN = os.environ.get("6779644697:AAEnepufjWmGYHeqENL28mb0ktP02a4F3X4", "")
+
+    APP_ID = int(os.environ.get("25217515", "")
+
+    API_HASH = os.environ.get("1bb27e5be73593e33fc735c1fbe0d855", "")
